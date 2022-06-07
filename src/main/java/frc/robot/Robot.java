5aecbcf4eaf513addd9e1884ef0f591988e9ca22
dcipicchio <dac221@pitt.edu>
2022-06-07 19:08:27 -0400

    added color sensor to robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,14 @@
 
 package frc.robot;
 
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -19,12 +26,24 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  private I2C.Port port = I2C.Port.kOnboard;
+
+  private ColorSensorV3 colorSensor = new ColorSensorV3(port);
+  private ColorMatch colorMatch = new ColorMatch();
+
+  private Color firstBlue = Color.kFirstBlue;
+  private Color firstRed = Color.kFirstRed;
+  private Color firstYellow = Color.kYellow;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
+    colorMatch.addColorMatch(firstBlue);
+    colorMatch.addColorMatch(firstRed);
+    colorMatch.addColorMatch(firstYellow);
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
@@ -44,6 +63,15 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+
+    ColorMatchResult colorResult = colorMatch.matchClosestColor(colorSensor.getColor());
+    if(colorResult.color == firstRed){
+      System.out.println("Red");
+    }else if(colorResult.color == firstBlue){
+      System.out.println("Blue");
+    }else if(colorResult.color == firstYellow){
+      System.out.println("Yellow");
+    }
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
